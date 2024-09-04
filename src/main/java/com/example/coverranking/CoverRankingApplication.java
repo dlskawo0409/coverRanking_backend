d295a390f985332c9b3ef5e7c24d1fb2eb66327b
dlskawo0409 <dlskawo0409@gmail.com>
2024-09-04 09:29:13 +0900

    init

---
+++ src/main/java/com/example/coverranking/CoverRankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.coverranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverRankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoverRankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coverranking/CoverRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coverranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverRankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
