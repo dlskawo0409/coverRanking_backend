f700f29a62e9ab475500274148445e8ed3b66c74
dlskawo0409 <dlskawo0409@naver.com>
2024-10-21 13:35:03 +0900

    Fix : SecurityConfig cors

---
+++ src/main/java/com/example/coverranking/member/dto/request/MemberNickName.java
@@ -0,0 +1,16 @@
+package com.example.coverranking.member.dto.request;
+import lombok.Builder;
+import lombok.Getter;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor   // 기본 생성자 추가
+@AllArgsConstructor  // 모든 필드를 받는 생성자 추가
+public class MemberNickName {
+    private String nickName;
+}
